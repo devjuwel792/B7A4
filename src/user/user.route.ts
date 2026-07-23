f4b1088dab40ee94440459e775af8bd1dc45971e
devjuwel792 <developerjuwel792@gmail.com>
2026-07-23 22:46:22 +0600

    feat: add authentication middleware to admin routes for enhanced security

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { AdminController } from "./user.controller";
+import authMiddleware from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.get("/users", AdminController.getAllUsers);
-router.patch("/users/:id", AdminController.updateUserStatus);
-router.get("/properties", AdminController.getAllProperties);
-router.get("/rentals", AdminController.getAllRentals);
+router.get("/users",authMiddleware, AdminController.getAllUsers);
+router.patch("/users/:id",authMiddleware, AdminController.updateUserStatus);
+router.get("/properties",authMiddleware, AdminController.getAllProperties);
+router.get("/rentals",authMiddleware, AdminController.getAllRentals);
 
 export const adminRoutes = router;
